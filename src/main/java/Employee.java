1ebd177395285c16d2f13b29d3548a2838da69f2
Carlos Balladares <carlos@balladares.co>
2018-07-26 03:42:20 -0700

    added annotations support

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,10 +1,23 @@
 /**
  * Created by carlosballadares on 2018-07-25.
  */
+import javax.persistence.*;
+
+@Entity
+@Table(name = "EMPLOYEE")
 public class Employee {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+  
+    @Column(name = "salary")
     private int salary;
 
     public Employee() {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,22 +7,22 @@ public class Main {
         ManageEmployee ME = new ManageEmployee();
         Integer id = ME.addEmployee("hello", "world", 10);
         System.out.println("New employye with id: "+id);
-        ME.listEmployees();
-        Integer empID1 = ME.addEmployee("Zara", "Ali", 1000);
-        Integer empID2 = ME.addEmployee("Daisy", "Das", 5000);
-        Integer empID3 = ME.addEmployee("John", "Paul", 10000);
+        // ME.listEmployees();
+        // Integer empID1 = ME.addEmployee("Zara", "Ali", 1000);
+        // Integer empID2 = ME.addEmployee("Daisy", "Das", 5000);
+        // Integer empID3 = ME.addEmployee("John", "Paul", 10000);
   
-        /* List down all the employees */
-        ME.listEmployees();
+        // /* List down all the employees */
+        // ME.listEmployees();
   
-        /* Update employee's records */
-        ME.updateEmployee(empID1, 5000);
+        // /* Update employee's records */
+        // ME.updateEmployee(empID1, 5000);
   
-        /* Delete an employee from the database */
-        ME.deleteEmployee(empID2);
+        // /* Delete an employee from the database */
+        // ME.deleteEmployee(empID2);
   
-        /* List down new list of the employees */
-        ME.listEmployees();
+        // /* List down new list of the employees */
+        // ME.listEmployees();
 
 
         System.exit(0);
